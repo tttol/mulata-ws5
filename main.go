6f992fd501768f95ee5760f5cb3e419ca7ca69de
t-takahashi <tttol.t4k.b1z@gmail.com>
2023-10-08 09:11:02 +0900

    initial commit

---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		return true // この部分は実際の運用時には安全なオリジンチェックを実装するべきです。
+	},
+}
+
+func handleConnection(w http.ResponseWriter, r *http.Request) {
+	// HTTP接続をWebSocketにアップグレード
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		fmt.Println("Error during connection upgrade:", err)
+		return
+	}
+	defer ws.Close()
+
+	fmt.Println("Client Connected!")
+	for {
+		messageType, msg, err := ws.ReadMessage()
+		if err != nil {
+			fmt.Println("Error during message reading:", err)
+			break
+		}
+		fmt.Println("Received:", string(msg))
+		err = ws.WriteMessage(messageType, msg)
+		if err != nil {
+			fmt.Println("Error during message writing:", err)
+			break
+		}
+	}
+}
+
+func main() {
+	http.HandleFunc("/ws", handleConnection)
+
+	fmt.Println("WebSocket server started on :5000...")
+	http.ListenAndServe(":5000", nil)
+}
