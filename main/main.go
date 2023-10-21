3fddac42ba0f740344967d12fd812ec015697e77
t-takahashi <tttol.t4k.b1z@gmail.com>
2023-10-21 18:19:44 +0900

    use gin

--- main/main.go
+++ main/main.go
@@ -4,15 +4,26 @@ import (
 	"log/slog"
 	"net/http"
 
+	"github.com/gin-gonic/gin"
 	"github.com/tttol/mulata-ws5/aws"
 )
 
 func main() {
 	http.HandleFunc("/ws", aws.HandleConnection)
 
-	slog.Info("WebSocket server started on :3001...")
-	err := http.ListenAndServe(":3001", nil)
-	if err != nil {
-		slog.Error("ListenAndServe error on port=3001:", err)
+	// Run WebSocket server in a goroutine
+	go func() {
+		slog.Info("WebSocket server started on :3001...")
+		if err := http.ListenAndServe(":3001", nil); err != nil {
+			slog.Error("ListenAndServe error on port 3001:", err)
+		}
+	}()
+
+	// Initialize Gin engine
+	ginEngine := gin.Default()
+	ginEngine.StaticFile("/", "./static/index.html")
+	if err := ginEngine.Run(":8000"); err != nil {
+		slog.Error("Failed to start Gin server:", err)
 	}
+	slog.Info("HTTP server started on :8000...")
 }
